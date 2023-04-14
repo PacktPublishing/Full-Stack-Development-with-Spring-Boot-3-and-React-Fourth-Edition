7116809359c6f850bc4f4c6d78342e568a41e7b7
juhahinkula <hinkula@gmail.com>
2023-04-14 08:31:37 +0300

    Chapter 1 added

---
+++ Chapter01/src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.cardatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
