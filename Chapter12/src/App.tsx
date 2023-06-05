8046a1f16dd4e9b4e4165d258d00686fe5fddee7
juhahinkula <hinkula@gmail.com>
2023-06-05 13:45:07 +0300

    Chapter 12 added

---
+++ Chapter12/src/App.tsx
@@ -0,0 +1,22 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+
+function App() {
+  return (
+    <Container maxWidth="xl">
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            Carshop
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Container>
+  );
+}
+
+export default App;
