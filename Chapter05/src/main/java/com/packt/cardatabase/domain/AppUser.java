1efbf4c86a7bf1dab5048879d858974d91b9ca4f
Juha Hinkula <hinkula@gmail.com>
2023-08-09 13:41:59 +0300

    Update AppUser.java

--- Chapter05/src/main/java/com/packt/cardatabase/domain/AppUser.java
+++ Chapter05/src/main/java/com/packt/cardatabase/domain/AppUser.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 @Entity
 public class AppUser {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(nullable = false, updatable = false)
 	private Long id;
 
