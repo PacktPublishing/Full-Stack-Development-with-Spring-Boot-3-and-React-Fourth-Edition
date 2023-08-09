1978db02940671a82d2c4be8b21d40f369b6a2b6
Juha Hinkula <hinkula@gmail.com>
2023-08-09 13:44:09 +0300

    Update SecurityConfig.java

--- Chapter05/src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ Chapter05/src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
