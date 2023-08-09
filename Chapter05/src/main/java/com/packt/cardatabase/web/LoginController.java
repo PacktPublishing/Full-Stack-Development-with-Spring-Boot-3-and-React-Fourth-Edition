8bd2667aca4b70b125192d8839b85cf186ded8d2
Juha Hinkula <hinkula@gmail.com>
2023-08-09 13:45:02 +0300

    Update LoginController.java

--- Chapter05/src/main/java/com/packt/cardatabase/web/LoginController.java
+++ Chapter05/src/main/java/com/packt/cardatabase/web/LoginController.java
@@ -2,7 +2,6 @@ package com.packt.cardatabase.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
