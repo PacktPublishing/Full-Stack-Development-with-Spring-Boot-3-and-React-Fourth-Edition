753a526713688f08d38e1b5be0027b0cb0873377
Juha Hinkula <hinkula@gmail.com>
2023-08-31 10:20:11 +0300

    Update CarController.java

--- Chapter06/src/main/java/com/packt/cardatabase/web/CarController.java
+++ Chapter06/src/main/java/com/packt/cardatabase/web/CarController.java
@@ -1,6 +1,5 @@
 package com.packt.cardatabase.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.packt.cardatabase.domain.Car;
@@ -8,9 +7,12 @@ import com.packt.cardatabase.domain.CarRepository;
 
 @RestController
 public class CarController {
-	@Autowired
-	private CarRepository repository;
+	private final CarRepository repository;
 
+	public CarController(CarRepository repository) {
+        	this.repository = repository;
+	}
+	
 	@GetMapping("/cars")
 	public Iterable<Car> getCars() {
 		return repository.findAll();
